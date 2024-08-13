92e2c20f4b6e6eaa96ba584730442c8e2a170b2d
JunhaumHayden <junhaumhayden@hotmail.com>
2024-08-12 21:32:46 -0300

    Update homepage file in Lista04 folder

--- basics/Lista03/exerc04/homepage.php
+++ basics/Lista03/exerc04/homepage.php
@@ -11,7 +11,7 @@
     <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
     <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
     
-    <script src="06_exerc.js"></script>
+    <script src="script.js"></script>
     <!-- <script>
       $( function() 
       {
