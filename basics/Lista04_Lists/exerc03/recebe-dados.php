c264a20e13355209588ed32e0ae481206167460a
pendrive <junhaumhayden@hotmail.com>
2024-08-21 11:36:13 -0300

    Update exerc02 in list folder

--- basics/Lista04_Lists/exerc03/recebe-dados.php
+++ basics/Lista04_Lists/exerc03/recebe-dados.php
@@ -8,10 +8,12 @@
         <link rel="stylesheet" href="style.css">
         <link rel="shortcut icon" type="imagex/png" href="/web/icons/icon.ico">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;1,200;1,400&family=Red+Hat+Display:wght@400;500;700&family=Roboto&display=swap"
-    rel="stylesheet">  
+    rel="stylesheet">
+
+        
     </head>
     <body>
-        <h1 id="idcabelho">  Tratamento de vetores em PHP - Lista 04 - Exercicio 02<br><span class="blinking-text">Resposta do Servidor</span></h1>
+        <h1 id="idcabelho">  Tratamento de vetores em PHP - Lista04 - Exercicio 02<br><span class="blinking-text">Resposta do Servidor</span></h1>
         <nav>
             <ul>
                 <li><a href="/web/index.html">Home</a></li>
@@ -32,14 +34,12 @@
                 // );
                 // Ou dessa maneira
                 $vetorAlunos = [
-                    $_POST['nome-aluno01'] => floatval($_POST['nota01']), 
-                    $_POST['nome-aluno02'] => floatval($_POST['nota02']), 
-                    $_POST['nome-aluno03'] => floatval($_POST['nota03'])
+                    $_POST['nome-aluno01'] => $_POST['nota01'], 
+                    $_POST['nome-aluno02'] => $_POST['nota02'], 
+                    $_POST['nome-aluno03'] => $_POST['nota03']
                 ];
             }
 
-            // Mostrar os dados em formato tabular
-            echo "<h3>Notas dos Alunos</h3>";
             echo "<table>
                     <tr>
                         <th> Nome do Aluno</th>
@@ -53,15 +53,6 @@
                       </tr>";
             }
             echo "</table>";
-
-            // Encontrar o aluno com a maior nota
-        $nomeMaiorNota = array_keys($vetorAlunos, max($vetorAlunos))[0];
-        $maiorNota = $vetorAlunos[$nomeMaiorNota];
-
-        // Mostrar o nome e a nota do aluno com a maior nota
-        echo "<h3>Aluno com Maior Nota</h3>";
-        echo "Nome: {$nomeMaiorNota}<br>";
-        echo "Nota: {$maiorNota}";
 
         ?>
         
