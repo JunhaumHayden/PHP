11633960f53ff0343d67e83ce7a88913d8708839
JunhaumHayden <junhaumhayden@hotmail.com>
2024-10-10 12:10:53 -0300

    Upgrade web server files

--- basics/Lista03_SimpleVariables/06_Calcular_Comissao/recebe-dados.php
+++ basics/Lista03_SimpleVariables/06_Calcular_Comissao/recebe-dados.php
@@ -40,42 +40,26 @@
             
         }
     ?>
+     <footer>
+            <div class="footer-container">
+                <div class="logo">
+                    <img src="/PHP/assets/icons/icon36x36.ico" width="30" alt="Logo">
+                </div>
+                <div class="info">
+                    <div>&copy; 2024 HTML / CSS / JavaScript / PHP</div>
+                    <div>Desenvolvido por: Carlos Hayden Junior</div>
+                </div>
+                <div class="icons">
+                    <img src="/PHP/assets/icons/icon-html5-48.png" width="20" alt="HTML Icon">
+                    <img src="/PHP/assets/icons/icon-css3-48.ico" width="20" alt="CSS Icon">
+                    <img src="/PHP/assets/icons/icons8-js-48.png" width="20" alt="JS Icon">
+                    <img src="/PHP/assets/icons/icons-php-48.ico" width="20" alt="PHP Icon">
+                    
+                </div>
+            </div>
+        </footer>
         
     </body>
-
-    <footer>
-        <table>
-            <tr> 
-                <td>
-                    <div class="logo">
-                        <img src="/web/icons/icon36x36.ico" width="30" alt="Logo">
-                    </div>    
-                </td> 
-                <td>
-                    <div class="info">
-                        <div>&copy; 2024 HTML / CSS / JavaScript</div>
-                        <div>Desenvolvido por: Carlos Hayden Junior</div>
-                    </div>
-                </td> 
-                <td>
-                    <div class="icons">
-                        <img src="/web/icons/icon-html5-48.png" width="20" alt="HTML Icon">
-                    </div>
-                </td> 
-                <td>
-                    <div class="icons">
-                        <img src="/web/icons/icon-css3-48.ico" width="20" alt="CSS Icon">
-                    </div>
-                </td> 
-                <td>
-                    <div class="icons">
-                        <img src="/web/icons/icons8-js-48.png" width="20" alt="JS Icon">
-                    </div>
-                </td> 
-            </tr>
-        </table>
-    </footer> 
-    
     </html>
     
    
